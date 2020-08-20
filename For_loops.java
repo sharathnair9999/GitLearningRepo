448130ff1a5bb9eaa01232fd3397ff1893190dbe
Sharath Nair <sharathnair9999@gmail.com>
2020-08-20 11:02:25 +0530

    updated for loops

--- For_loops.java
+++ For_loops.java
@@ -3,5 +3,7 @@ public static void main(String[] args){
 	for(int i=0;i<10;i++){
 		System.out.println("Hello");
 	}
+	System.out.println("done with for loops");
+	System.out.println("Where is while loops");
    }
 }
